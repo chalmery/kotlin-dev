f66bc3838a367f0122430b69ea4a08cbbb731fc6
chalmery <95135709+chalmery@users.noreply.github.com>
2024-05-11 23:47:00 +0800

    4.5 Retrofit请求

---
+++ src/main/kotlin/L2/RetrofitApp.kt
@@ -0,0 +1,57 @@
+package L2
+
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.io.File
+
+
+interface GitHubApi {
+
+
+    @GET("/repos/{owner}/{repo}")
+    fun getRepository(@Path("owner") user: String, @Path("repo") repo: String): Call<Repository>
+}
+
+
+fun main() {
+
+    val gitHubApi = Retrofit.Builder().baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(GitHubApi::class.java)
+
+    val res = gitHubApi.getRepository("Jetbrains", "kotlin").execute()
+    println(Gson().toJson(res.body()))
+
+    if (res.body() == null) {
+        println(res.message())
+        return
+    }
+
+    val repo = res.body()
+
+
+    File("kotlin.html").writeText(
+        """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>${repo?.owner?.login} - ${repo?.name}</title>
+        </head>
+        <body>
+            <h1><a href='${repo?.html_url}'>${repo?.owner?.login} - ${repo?.name}</a></h1>
+            <p>${repo?.description}</p>
+            <p>${repo?.stargazers_count}</p>
+            <p>${repo?.forks_count}</p>
+        </body>
+        </html>
+    """.trimIndent()
+    )
+
+
+}
